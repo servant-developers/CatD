dbff148ad564e0d1e7dda504190ba11c124f36bd
youngje jo <youngje4284@woowahan.com>
2020-09-09 00:37:06 +0900

    feat: eslint stylelint husky prettier setting

--- src/Button/Button.test.tsx
+++ src/Button/Button.test.tsx
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom/extend-expect';
 
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import { Button } from './Button';
 

97403d510875e6fcd6b55e8ae1b6ab58fa8db07f
andy nam <naamoonoo.y@gmail.com>
2020-09-20 21:51:30 +0900

    feat: update storybook based on updating button component

--- src/Button/Button.stories.tsx
+++ src/Button/Button.stories.tsx
@@ -10,34 +10,37 @@ export default {
     componentSubtitle: '연습용 버튼입니다',
   },
   component: Button,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 } as Meta;
 
 const Template: Story<ButtonProps> = (args: ButtonProps) => (
   <Button {...args} onClick={action('button clicked')} />
 );
 
+export const Default = Template.bind({});
+Default.args = {
+  text: 'Default',
+} as ButtonProps;
+
 export const Primary = Template.bind({});
 Primary.args = {
-  primary: true,
-  label: 'Button',
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Secondary',
-};
+  type: 'primary',
+  text: 'Primary',
+} as ButtonProps;
 
 export const Large = Template.bind({});
 Large.args = {
   size: 'large',
-  label: 'Large',
-};
+  text: 'Large',
+} as ButtonProps;
 
 export const Small = Template.bind({});
 Small.args = {
   size: 'small',
-  label: 'Small',
-};
+  text: 'Small',
+} as ButtonProps;
+
+export const Full = Template.bind({});
+Full.args = {
+  text: 'Full',
+  hasFullWidth: true,
+} as ButtonProps;
